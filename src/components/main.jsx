1c26b95b24609fd2a18cab8d13df3651ed7cee9b
Ziomeczek <igordukiewicz@gmail.com>
2026-04-16 22:42:26 +0200

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -220,7 +220,7 @@ function MainS() {
                                 <p className="textl">✔ Do 2 podstron</p>
                                 <p className="textl">✔ Podstawowy formularz kontaktowy</p>
                                 <p className="textl">✔ Responsywność strony</p>
-                                <p className="textl">✔ Czas realizacji do 5 dni</p>
+                                <p className="textl">✔ Czas realizacji ok.5 dni</p>
                                 </div>
                                 <a href="#kontakt"><button className="btnof">Kontakt</button></a>
                             </div>
@@ -232,7 +232,7 @@ function MainS() {
                                 <p className="textl">✔ Do 5 podstron </p>
                                 <p className="textl">✔ Responsywność </p>
                                 <p className="textl">✔ Podpięcie pod domenę i hosting</p>
-                                <p className="textl">✔ Czas realizacji do 7 dni</p>
+                                <p className="textl">✔ Czas realizacji ok.7 dni</p>
                                 </div>
                                 <a href="#kontakt"><button className="btnof">Kontakt</button></a>
 
